944e8bba4917324dc963d396f3d64c841e2c6f03
Ing. Jan Kaláb <pitlicek@gmail.com>
2013-01-04 21:27:16 +0100

    Why is the text field still empty?! :(

--- src/cz/pitel/glslnfc/EditorFragment.java
+++ src/cz/pitel/glslnfc/EditorFragment.java
@@ -11,11 +11,18 @@ import android.widget.EditText;
 public class EditorFragment extends Fragment {
 	@Override
 	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+		/*
 		final View view = inflater.inflate(R.layout.editor, container, false);
 		final EditText editor = (EditText) view.findViewById(R.id.editor);
 		editor.setHorizontallyScrolling(true);
 		//editor.setText(getActivity().getPreferences(Context.MODE_PRIVATE).getString("shader", "blah"));
 		editor.setText("java");
-		return view;
+		*/
+		return inflater.inflate(R.layout.editor, container, false);
+	}
+
+	public void onResume() {
+		((EditText) getActivity().findViewById(R.id.editor)).setText("java");
+		((EditText) getView().findViewById(R.id.editor)).setText("java");
 	}
 }
