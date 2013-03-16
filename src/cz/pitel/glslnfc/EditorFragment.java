eb8af4ac4956077582a430d63bab1d9db6948920
Ing. Jan Kaláb <pitlicek@gmail.com>
2013-03-16 17:16:51 +0100

    Text watcher

--- src/cz/pitel/glslnfc/EditorFragment.java
+++ src/cz/pitel/glslnfc/EditorFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,18 @@ public class EditorFragment extends Fragment {
 		final View view = inflater.inflate(R.layout.editor, container, false);
 		final EditText editor = (EditText) view.findViewById(R.id.editor);
 		editor.setHorizontallyScrolling(true);	//Does not work from XML
+		editor.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void afterTextChanged(final Editable s) {
+				Log.v("GLSL", s.toString());
+			}
+
+			@Override
+			public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {}
+
+			@Override
+			public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {}
+		});
 		return view;
 	}
 
